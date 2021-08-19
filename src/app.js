0cbb9aee5c6ad0577dec29ed4cddfb2917bcb4ca
elizajanecook <elizajanecook@gmail.com>
2021-08-19 20:03:50 +1000

    added JS template to forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img
+    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+    alt=""
+    width="40"
+    />
+    <div class="weather-forecast-temperatures">
+    <span class="weather-forcast-max">18°</span>
+    <span class="weather-forcast-min">12°</span>
+    </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -85,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Brisbane");
+displayForecast();
